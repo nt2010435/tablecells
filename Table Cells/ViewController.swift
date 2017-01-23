504208ec36cbfd305def56ec042bd49a7edc22cd
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2017-01-23 08:05:29 -0800

    Added codes using xib file

--- Table Cells/ViewController.swift
+++ Table Cells/ViewController.swift
@@ -2,24 +2,55 @@
 //  ViewController.swift
 //  Table Cells
 //
-//  Created by Nicholas Tran on 1/19/17.
-//  Copyright © 2017 NT Dev. All rights reserved.
+//  Created by Kim Topley on 9/12/15.
+//  Copyright © 2015 Apress Inc. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    let cellTableIdentifier = "CellTableIdentifier"
+    @IBOutlet var tableView:UITableView!
+    let computers = [
+        ["Name" : "MacBook Air", "Color" : "Silver"],
+        ["Name" : "MacBook Pro", "Color" : "Silver"],
+        ["Name" : "iMac", "Color" : "Silver"],
+        ["Name" : "Mac Mini", "Color" : "Silver"],
+        ["Name" : "Mac Pro", "Color" : "Black"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let nib = UINib(nibName: "NameAndColorCell", bundle: nil)
+        tableView.register(nib,
+                    forCellReuseIdentifier: cellTableIdentifier)
+        tableView.rowHeight = 65
+   }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView,
+                numberOfRowsInSection section: Int) -> Int {
+        return computers.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                cellForRowAt indexPath: IndexPath)
+                        -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+                        withIdentifier: cellTableIdentifier, for: indexPath)
+                        as! NameAndColorCell
+        
+        let rowData = computers[indexPath.row]
+        cell.name = rowData["Name"]!
+        cell.color = rowData["Color"]!
+        
+        return cell
+    }
 }
 
